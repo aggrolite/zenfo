fcb505686110d3a39926732f86ecb520fdcc6593
Curtis Brandt <curtis@zenfo.info>
2018-12-22 18:26:00 -0800

    Print usage for required options

--- cmd/zenfo-build/main.go
+++ cmd/zenfo-build/main.go
@@ -17,6 +17,15 @@ func init() {
 	flag.StringVar(&dbName, "dbname", os.Getenv("DBNAME"), "Postgres DB name")
 	flag.StringVar(&dbUser, "dbuser", os.Getenv("DBUSER"), "Postgres DB user")
 	flag.Parse()
+
+	if len(dbName) == 0 {
+		flag.Usage()
+		os.Exit(2)
+	}
+	if len(dbUser) == 0 {
+		flag.Usage()
+		os.Exit(2)
+	}
 }
 
 func main() {
