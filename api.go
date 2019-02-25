3f4c0a864362d73f700ef3213aebbeeee5d6e6f0
Curtis Brandt <curtis@zenfo.info>
2019-02-25 08:54:58 -0800

    Support different styles of event calls

--- api.go
+++ api.go
@@ -12,10 +12,13 @@ import (
 
 // API provides HTTP endpoints for serving events
 type API struct {
-	dev  bool
-	dbh  *sql.DB
-	cert string
-	key  string
+	dev             bool
+	dbh             *sql.DB
+	cert            string
+	key             string
+	eventsAllStmt   *sql.Stmt
+	eventsOneStmt   *sql.Stmt
+	eventsAfterStmt *sql.Stmt
 }
 
 // NewAPI returns new API object
@@ -25,13 +28,28 @@ func NewAPI(dbUser, dbName, cert, key string, dev bool) (*API, error) {
 		cert: cert,
 		key:  key,
 	}
-	if !a.dev {
-		db, err := sql.Open("postgres", fmt.Sprintf("user=%s dbname=%s sslmode=disable", dbUser, dbName))
-		if err != nil {
-			return nil, err
-		}
-		a.dbh = db
+	db, err := sql.Open("postgres", fmt.Sprintf("user=%s dbname=%s sslmode=disable", dbUser, dbName))
+	if err != nil {
+		return nil, err
 	}
+	a.dbh = db
+
+	q := "SELECT id, venue_id, name, blurb, description, start_date, end_date, url FROM events"
+
+	var stmtErr error
+	a.eventsAllStmt, stmtErr = a.dbh.Prepare(q)
+	if stmtErr != nil {
+		return nil, stmtErr
+	}
+	a.eventsOneStmt, stmtErr = a.dbh.Prepare(fmt.Sprintf("%s WHERE id = $1", q))
+	if stmtErr != nil {
+		return nil, stmtErr
+	}
+	a.eventsAfterStmt, stmtErr = a.dbh.Prepare(fmt.Sprintf("%s WHERE start_date > $1", q))
+	if stmtErr != nil {
+		return nil, stmtErr
+	}
+
 	return a, nil
 }
 
@@ -83,10 +101,7 @@ func (api *API) redirect(w http.ResponseWriter, req *http.Request) {
 
 // Close closes DB handler
 func (api *API) Close() error {
-	if api.dev {
-		return api.dbh.Close()
-	}
-	return nil
+	return api.dbh.Close()
 }
 
 func (api *API) getVenues(w http.ResponseWriter, r *http.Request) {
@@ -125,21 +140,40 @@ func (api *API) getHealth(w http.ResponseWriter, r *http.Request) {
 func (api *API) getEvents(w http.ResponseWriter, r *http.Request) {
 	api.setHeaders(w.Header())
 
-	q := "SELECT id, venue_id, name, blurb, description, start_date, end_date, url FROM events"
+	q := r.URL.Query()
 
-	keys, ok := r.URL.Query()["id"]
-	if ok {
-		q = fmt.Sprintf("%s WHERE id=%s", q, keys[0])
+	var (
+		events []*Event
+		err    error
+	)
+	for param, v := range q {
+		if param == "id" {
+			events, err = api.events(api.eventsOneStmt, v[0])
+			break
+		}
+
+		if param == "after" {
+			events, err = api.events(api.eventsAfterStmt, v[0])
+			break
+
+		}
 	}
 
-	q = fmt.Sprintf("%s ORDER BY start_date", q)
+	if len(q) == 0 {
+		events, err = api.events(api.eventsAllStmt)
+	}
 
-	// Fetch events
-	rows, err := api.dbh.Query(q)
 	if err != nil {
-		log.Printf("url=%s err=%s q=%s\n", r.URL, err, q)
-		http.Error(w, "Oops! Something went wrong!", http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
+	}
+	json.NewEncoder(w).Encode(events)
+}
+
+func (api *API) events(stmt *sql.Stmt, args ...interface{}) ([]*Event, error) {
+	rows, err := stmt.Query(args...)
+	if err != nil {
+		return nil, err
 	}
 	defer rows.Close()
 
@@ -150,8 +184,7 @@ func (api *API) getEvents(w http.ResponseWriter, r *http.Request) {
 			event   Event
 		)
 		if err := rows.Scan(&event.ID, &venueID, &event.Name, &event.Blurb, &event.Desc, &event.Start, &event.End, &event.URL); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
+			return nil, err
 		}
 
 		// Fetch venue tied to venue_id
@@ -159,15 +192,10 @@ func (api *API) getEvents(w http.ResponseWriter, r *http.Request) {
 		venueRow := api.dbh.QueryRow(`SELECT id, name, addr, geo[0], geo[1], website, phone, email FROM venues WHERE id=$1`, venueID)
 		err := venueRow.Scan(&venue.ID, &venue.Name, &venue.Addr, &venue.Lat, &venue.Lng, &venue.Website, &venue.Phone, &venue.Email)
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
+			return nil, err
 		}
 		event.Venue = venue
 		events = append(events, &event)
 	}
-	if err := rows.Err(); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	json.NewEncoder(w).Encode(events)
+	return events, rows.Err()
 }
