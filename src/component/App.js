f8911bdd9783874ada15e2e6e4953da447fdd5e0
Curtis Brandt <curtis@zenfo.info>
2019-02-27 20:41:15 -0800

    new font

--- src/component/App.js
+++ src/component/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import WebFont from 'webfontloader';
 
 import About from './About'
 import Calendar from './Calendar'
 import Event from './Event'
 import Menu from './Menu'
+
+WebFont.load({
+  google: {
+    families: ['Fira Mono', 'monospace']
+  }
+});
 
 class App extends React.Component {
   render() {
