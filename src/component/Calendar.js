cfa239a15f54bf072cc5fea8f3e5b8ef62bd33df
Curtis Brandt <curtis@zenfo.info>
2019-02-28 20:02:14 -0800

    Infinite scrolling

--- src/component/Calendar.js
+++ src/component/Calendar.js
@@ -27,22 +27,25 @@ class Calendar extends React.Component {
     }
   }
 
-  onScroll() {
-
+  onScroll(e) {
+    e.preventDefault()
     const {
       loading,
       events,
     } = this.state;
 
-    if (loading) return;
-    var lastLi = document.querySelector("ul > li.event:last-child");
-    var lastLiOffset = lastLi.offsetTop + lastLi.clientHeight;
-    var pageOffset = window.pageYOffset + window.innerHeight;
-    if (pageOffset > lastLiOffset) {
-      let start = events.slice(-1)[0].start
-      this.loadEvents(start)
-    }
 
+    if (loading) return;
+
+    var scrollTop = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+    var scrollHeight = (document.documentElement && document.documentElement.scrollHeight) || document.body.scrollHeight;
+    var clientHeight = document.documentElement.clientHeight || window.innerHeight;
+    var scrolledToBottom = Math.ceil(scrollTop + clientHeight) >= scrollHeight;
+
+    let start = events.slice(-1)[0].start
+    if (scrolledToBottom) {
+      this.loadEvents(start);
+    }
   }
 
   loadEvents(after) {
@@ -56,38 +59,31 @@ class Calendar extends React.Component {
         fetch(eventsURL).then((resp) => resp.json()),
         fetch(API.BASE_URL + "/venues").then((resp) => resp.json())
       ]).then(resp => {
-        var newEvents = this.state.events.slice().concat(resp[0])
+        //var newEvents = this.state.events.slice().concat(resp[0])
         this.setState({
-          events: newEvents,
-          venues: resp[1],
-          loading: false
+          events: [...this.state.events, ...resp[0] ],
+          //venues: resp[1],
+          loading: false,
         });
       })
   }
 
   componentDidMount() {
-    window.addEventListener('scroll', this.onScroll, false);
+    window.addEventListener('scroll', this.onScroll)
     this.loadEvents()
   }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.onScroll)
+  }
+
   render() {
 
-    const { events, venues, loading } = this.state;
-
-    if (loading) {
-      return <p>Loading...</p>
-    }
+    const { events, loading } = this.state;
 
 	return (
       <div className="calendar">
-        <div className="venues">
-          {venues.map(venue =>
-              <div className="venue" key={venue.id}>
-                <input type="checkbox" checked={true}/>
-                <label htmlFor={venue.id}>{venue.name}</label>
-              </div>
-          )}
-        </div>
-        <ul>
+        <ul className="ul" >
           {events.map(event =>
             <li className="event" key={event.id}>
               <div className="date">
@@ -109,6 +105,9 @@ class Calendar extends React.Component {
               </div>
             </li>
           )}
+          {loading &&
+            <li>Loading...</li>
+          }
         </ul>
 
       </div>
